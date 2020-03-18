5b0371fa3da00e4ea26d8f5ca0f6ad08d4b0b6fb
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2020-03-18 18:16:53 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.get("/url", (req, res, next) => {
+    res.json(["keep","the","door","open","3-inches"]);
+   });
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
